ded2110f6288629e3b03b68b8c0c7c1bb944f2b7
emmanuelbilkis <emmanuelbilkis@gmail.com>
2022-06-17 18:22:29 -0300

    muestro el listado de archivos de la carpeta elegida

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TP8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string directorio= @"C:\taller2021\emma\repositorios";
+          List <string> listaDeArchivos = Directory.EnumerateFiles(directorio).ToList();
+
+          Console.WriteLine("Lista de archivos de mi directorio elegido");
+          foreach (string archivoX in listaDeArchivos)
+          {
+            Console.WriteLine(archivoX);
+          }
+
+          if (!File.Exists(directorio + @"\index.csv"))
+          {
+                File.Create(directorio + @"\index.csv");
+          }
+
+            
+
+
+
+
+
+          
+
+          
+        }
+    }
+}
+
